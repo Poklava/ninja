f98b0edbc45d3721f99db877454a3583258ca4cf
Poklava <66903746+Poklava@users.noreply.github.com>
2020-06-14 15:57:54 +0300

    Translation for TR
    some words have been changed so that they can be understood by TR people.
    
    Thanks for enjoyable module!

---
+++ translations/tr.lua
@@ -0,0 +1,43 @@
+translations.en = {
+    name = "tr",
+    lastTime = "Son süren: %ss",
+    lastBestTime = "En iyi süren: %ss",
+    helpToolTip = "<p align='center'>Yardım için <b>H</b> tuşuna bas.</p>",
+    optionsYes = "<font color='#53ba58'>Evet</font>",
+    optionsNo = "<font color='#f73625'>Hayır</font>",
+    graffitiSetting = "Grafitileri etkinleştir",
+    particlesSetting = "İleri atılma/Hava-sıçraması parçacıklarını etkinleştir",
+    timePanelsSetting = "Süre panelini etkinleştir",
+    globalChatSetting = "Evrensel sohbeti etkinleştir",
+    voteStart = "%s Mevcut haritayı değiştirmek için oy başlatıldı. Kabul etmek için !yes yazınız.",
+    newRecord = "<R>%s haritayı en kısa sürede bitirdi! %ss</R>",
+    devInfo = "<V>Harita göndermek ister misiniz? Bu bağlantıyı kontrol edin: https://atelier801.com/topic?f=6&t=888399</V>\n<font color='#CB546B'>Bu modül geliştirilme aşamasındadır. Lütfen hataları Extremq#0000 veya Railysse#0000'a bildirin.</font>",
+    discordInfo = "<BV>Discord kanalımıza katılın! https://discord.gg/WawZVaq</BV>",
+    welcomeInfo = " <font color='#E68D43'>#ninja</font>Odasına hoş geldiniz! Yardım için <font color='#E68D43'>H</font> tuşuna bas.",
+    finishedInfo = "Haritayı bitirdin! (<V>%ss</V>)",
+    helpBody = "Peyniri en hızlı şekilde deliğe geri getirmelisin.\n\n<b>Yetenekler</b>:\n» İleri atılma - İki defa <b><font color='#CB546B'>Sol</font></b> veya <b><font color='#CB546B'>Sağ Yön tuşlarına</font></b> bas. (Bekleme süresi 1s)\n» Hava sıçraması - İki defa <b><font color='#CB546B'>Yukarı Yön tuşuna</font></b> bas. (Bekleme süresi 2s)\n» Geri sarma - Kontrol noktası bırakmak için <b><font color='#CB546B'>Space</font></b> tuşuna basın. Kontrol noktasına geri dönmek için 3 saniye içinde <b><font color='#CB546B'>Space</font></b> tuşuna tekrar basın. (Bekleme süresi 10s)\n\n<b>Diğer kısayollar</b>:\n» Ölmek - Ölmek için <b><font color='#CB546B'>X</font></b> tuşuna basın veya /mort yazın.\n» Menü - Menüyü açmak için <b><font color='#CB546B'>M</font></b> veya ekranın sol kenarına tıklayın.\n» Grafiti - Grafiti bırakmak için <b><font color='#CB546B'>C</font></b> tuşuna basın. (Bekleme süresi 60s)\n» Yardım - Yardım ekranını açmak/kapamak için <b><font color='#CB546B'>H</font></b> tuşuna basın.\n\n<b>Komutlar</b>:\n» !p Name#id - Başka oyuncuların istatistiklerini kontrol etmek için kullanılır.\n» !pw Password - Odaya şifre koymak için kullanılır. (Oda sizin açmış olmanız gerekir.)\n» !m @code - istediğiniz haritayı yüklemek için kullanılır. (Oda şifreli olmak zorunda.)\n» !langue country - Modülün dilini değiştirir. (Yalnız sizin için)\n\n<p align='center'><a href='event:CloseMenu'><b><font color='#CB546B'>Kapat</font></b></a></p>", --23
+    Xbtn = "X",
+    -- IF YOUR LANGUAGE HAS SEPCIAL CHARACTERS, PLEASE JUST LEAVE THESE AS DEFAULT
+    shopTitle = "Koleksiyon",
+    profileTitle = "Profil",
+    leaderboardsTitle = "Lider tablosu",
+    settingsTitle = "Ayarlar",
+    aboutTitle = "Hakkında",
+    -- END
+    aboutBody = "Modül <font color='#FFD991'>Extremq#0000</font>tarafından kodlanmıştır.\nOynanış fikirleri, hata-testi ve haritalar  <font color='#FFD991'>Railysse#0000</font> tarafından sağlandı.\n\nBu modül „Red Cheese” fare vakfı tarafından „Save Module” projesi ile tamamen desteklenmektedir. Kazanacağımız tüm bağışlar #parkour oynayan farelere bağışlanacak, böylece modülümüzü oynamak için onlara rüşvet verebileceğiz.\n\nSadece şaka yapıyoruz, modülümüzü denediğiniz için teşekkür ederiz! :D\n\n<p align='center'><font color='#f73625'>&lt;3</font></p>", -- 30
+    playtime = "Oynama süresi",
+    firsts = "Birincilik",
+    finishedMaps = "Tamamlanan haritalar",
+    firstRate = "Birincilik oranı",
+    holeEnters = "Deliğe girme sayısı",
+    graffitiUses = "Kullanılan grafitiler",
+    dashUses = "İleri atılma sayısı",
+    rewindUses = "Geri sarma sayısı",
+    hardcoreMaps = "Tamamlanan zor haritalar",
+    shopNotice = "Mağaza geliştiriliyor.",
+    leaderboardsNotice = "Modül resmi hale geldiğinde lider tablosu eklenecektir.",
+    notValidCommand = "%s geçerli bir komut değil veya yanlış bir argüman yazdınız.",
+    cantSetPass = "Bu odaya şifre ayarlanamıyor.",
+    translator = "Poklava#0000 tarafından çevirildi.",
+    version = "Versiyon: %s"
+}
